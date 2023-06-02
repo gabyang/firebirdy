b0a08ad8009243568284657672e0dbb1fd6176a5
gabyang <e0725795@u.nus.edu>
2023-06-02 11:52:54 -0700

    first commit - basic camera magnetometer anaylsis

--- App.js
+++ App.js
@@ -1,12 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Magnetometer } from 'expo-sensors';
+import { Camera } from 'expo-camera';
+import * as Permissions from 'expo-permissions';
 
 export default function App() {
+  const [hasCameraPermission, setHasCameraPermission] = useState(null);
+  const [cameraRef, setCameraRef] = useState(null);
+  const [magnetometerData, setMagnetometerData] = useState({ x: 0, y: 0, z: 0 });
+  const [subscription, setSubscription] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Permissions.askAsync(Permissions.CAMERA);
+      setHasCameraPermission(status === 'granted');
+    })();
+  }, []);
+
+  useEffect(() => {
+    _subscribe();
+    return () => _unsubscribe();
+  }, []);
+
+  const _subscribe = () => {
+    setSubscription(
+      Magnetometer.addListener(result => {
+        setMagnetometerData(result);
+      })
+    );
+  };
+
+  const _unsubscribe = () => {
+    subscription && subscription.remove();
+    setSubscription(null);
+  };
+
+  const takePhoto = async () => {
+    if (cameraRef) {
+      const options = { quality: 0.5, base64: true };
+      const photo = await cameraRef.takePictureAsync(options);
+      console.log(photo.uri);
+    }
+  };
+
+  if (hasCameraPermission === null) {
+    return <View />;
+  }
+  if (hasCameraPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Camera style={styles.camera} ref={ref => setCameraRef(ref)} />
+      <TouchableOpacity onPress={takePhoto} style={styles.capture} activeOpacity={0.7}>
+        <Text>Take Photo</Text>
+      </TouchableOpacity>
+      <Text>Magnetometer:</Text>
+      <Text>x: {magnetometerData.x}</Text>
+      <Text>y: {magnetometerData.y}</Text>
+      <Text>z: {magnetometerData.z}</Text>
     </View>
   );
 }
@@ -14,8 +67,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  camera: {
+    flex: 1,
+  },
+  capture: {
+    flex: 0,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: 'center',
+    margin: 20,
   },
 });
